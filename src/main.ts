b2f215442cbcd3afa87c919b2e8c97b0fb0d3c29
AleQRQ <alessandro.curcu@saep-ict.it>
2020-05-22 11:48:19 +0200

    Aggiunge nuova funzione

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,5 @@
 // Ritorna una lista di elementi che esiste in entrambi gli array, usando una funzione comparatrice
 export const intersectionWith = (a: any[], b: any[], comp: (a: any, b: any) => boolean): any[] => a.filter(x => b.findIndex(y => comp(x, y)) !== -1)
+
+// Ritorna l'indice dell'ultimo elemento di un array per il quale la funzione fornita ritorna un valore truthy
+export const findLastIndex = (arr: any[], fn: (val: any, i: number, arr: any[]) => any): number | undefined => arr.map((val: any, i: number) => [i, val]).filter(([i, val]) => fn(val, i, arr)).pop()?.[0]
